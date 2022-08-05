d8b5cbed0ce69f90c41da3d76bb0873d26ddafa8
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-05 16:31:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+
+function writeCards(names, birthday) {
+    let arr = [];
+    for(let i=0; i < names.length; i+=1) {
+        arr.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`)
+    }
+    return arr 
+}
+function countDown(integer) {
+    let i = integer;
+    while (i >= 0) {
+        console.log(i);
+        i -= 1;
+    }
+}
